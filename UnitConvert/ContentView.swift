0acedbbbaabbc17b0e1641135b8287b9e24da948
Jay Shah <jayshah@yahoo.com>
2019-12-18 16:05:49 -0800

    Initial Commit

---
+++ UnitConvert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConvert
+//
+//  Created by Jaydev Shah on 12/18/19.
+//  Copyright © 2019 Jaydev Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
